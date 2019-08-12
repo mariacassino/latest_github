2ed7f0c7136506d5fa6a2c6ab88b50a733eba93d
Maria Cassino <maria.cassino@gmail.com>
2019-08-11 22:34:23 -0400

    created migration to drop Owner table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_190756) do
+ActiveRecord::Schema.define(version: 2019_08_11_194319) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
